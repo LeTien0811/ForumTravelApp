c75f555cde0de7b55c4d6bb67b531431d30f90b7
LeVietTien <leviettien@LeVietTien.LeVietTienDomain>
2025-11-08 08:06:43 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# travel_torum_app
+
+A new Flutter project.
---
+++ lib/core/config/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff040D12);
+  static const white = Color(0xffFFFFFF);
+  static const black = Color(0xff000000);
+}
