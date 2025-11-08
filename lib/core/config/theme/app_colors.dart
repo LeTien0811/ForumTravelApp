d6058ea896b49e5cd58344dea5d6a7f8aa8d3b51
LeVietTien <leviettien@LeVietTien.LeVietTienDomain>
2025-11-08 08:36:00 +0700

    push 2

--- lib/core/config/theme/app_colors.dart
+++ lib/core/config/theme/app_colors.dart
@@ -4,4 +4,7 @@ class AppColors {
   static const primary = Color(0xff040D12);
   static const white = Color(0xffFFFFFF);
   static const black = Color(0xff000000);
+  static const gray1 = Color(0xffEFEFEB);
+  static const gray2 = Color(0xffECE1E1);
+  static const blue = Color(0xff235DFF);
 }
